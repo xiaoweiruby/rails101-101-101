6637844bff96e7974959e21449cf6b34f490423d
xiaowei <shenzhoudance@gmail.com>
2019-10-17 20:32:57 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,13 @@ class GroupsController < ApplicationController
      redirect_to groups_path
  end
 
+   def destroy
+     @group = Group.find(params[:id])
+     @group.destroy
+     flash[:alert] = "Group deleted"
+     redirect_to groups_path
+   end
+
  private
 
  def group_params
