0a11bcc80753f76d7b695c73d18c754a17a470d8
xiaowei <shenzhoudance@gmail.com>
2019-10-17 20:54:01 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
 
   def index
     @groups = Group.all
@@ -19,8 +19,9 @@ class GroupsController < ApplicationController
 
 
 
-  def create
+ def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_123950) do
+ActiveRecord::Schema.define(version: 2019_10_17_125221) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
